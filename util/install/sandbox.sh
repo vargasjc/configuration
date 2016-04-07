c571e47177b60e07896cf1706910da65a9aa303b
vargasjc <vrg412lls@outlook.com>
2016-04-06 19:15:42 -0600

    Update sandbox.sh

--- util/install/sandbox.sh
+++ util/install/sandbox.sh
@@ -62,7 +62,7 @@ fi
 ## Clone the configuration repository and run Ansible
 ##
 cd /var/tmp
-git clone https://github.com/edx/configuration
+git clone https://github.com/vargasjc/configuration
 cd configuration
 git checkout $CONFIG_VER
 
